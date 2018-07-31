d38dc8e131100a510b225b40229694d5c6eb1e69
Nikolay Alekseev <nalekseev@i-kompas.ru>
2018-07-31 18:03:46 +0300

    add ExpertKeyHelper

---
+++ Tpr.Chat.Core/Helpers/ExpertKeyHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tpr.Chat.Core.Helpers
+{
+    public static class ExpertKeyHelper
+    {
+        public static string GetKey(Guid appealId, string nickname)
+        {
+            return HashHelper.Md5($"{appealId}{nickname}tprchat");
+        }
+    }
+}
