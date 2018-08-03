8494be4bb4a9a1f91fb176a312ad16bc06a183df
Ilya Malakhov <ilyamalahov@mail.ru>
2018-08-03 17:50:15 +0400

    201803081750

--- Tpr.Chat.Web/wwwroot/js/site.es5.js
+++ Tpr.Chat.Web/wwwroot/js/site.es5.js
@@ -2,12 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+"use strict";
+
 $(document).ready(function () {
     var accessToken = sessionStorage.getItem('access_token');
 
     if (!accessToken) {
-        $.ajax({ method: "POST", url: "token", data: { appealId: appealId, key: expertKey }, async: false })
-            .done(function (response) { sessionStorage.setItem("access_token", response.accessToken); })
-            .fail(function (xhr, status, error) { console.log(error); });
+        $.ajax({ method: "POST", url: "token", data: { appealId: appealId, key: expertKey }, async: false }).done(function (response) {
+            sessionStorage.setItem("access_token", response.accessToken);
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
     }
 });
+
